3bbdfd89be6236cac4d77079bb672d1d905a0548
JonathanSM-dev <jonathansilva.ans@gmail.com>
2022-09-27 15:43:27 -0300

    jform

---
+++ java_lessons/src/jform_designer/FirstAPI.java
@@ -0,0 +1,46 @@
+/*
+ * Created by JFormDesigner on Mon Sep 26 10:38:37 BRT 2022
+ */
+
+package jform_designer;
+
+import javax.swing.*;
+import net.miginfocom.swing.*;
+
+/**
+ * @author unknown
+ */
+public class FirstAPI extends JPanel {
+    public FirstAPI() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
+
+        //======== this ========
+        setLayout(new MigLayout(
+            "hidemode 3",
+            // columns
+            "[fill]" +
+            "[fill]" +
+            "[fill]" +
+            "[fill]" +
+            "[fill]" +
+            "[fill]" +
+            "[fill]" +
+            "[fill]",
+            // rows
+            "[]" +
+            "[]" +
+            "[]" +
+            "[]" +
+            "[]" +
+            "[]" +
+            "[]"));
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents  @formatter:on
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables  @formatter:off
+    // JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
+}
